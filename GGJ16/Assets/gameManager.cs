01bdd0618dad45c7d1786f46bea3dfd424a7c207
Hopptramp <rob-hopps@hotmail.co.uk>
2016-01-29 22:56:23 +0000

    game manager  + game reset

--- GGJ16/Assets/gameManager.cs
+++ GGJ16/Assets/gameManager.cs
@@ -1,23 +1,74 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class gameManager : MonoBehaviour
 {
     int score = 0;
+    int circle = 0;
     float timeElapsed = 0;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    public Text scoreText;
+    public Text circleText;
+    public Text timeText;
+
+    [SerializeField] int[] circleBoundaries;
+    [SerializeField] Sprite[] platformSprites;
+    [SerializeField] Sprite[] backgroundSprites;
+
+    LevelGeneration level;
+
+    // Use this for initialization
+    void Start ()
+    {
+        InvokeRepeating("calculateScore", 0, 1);
+
+        circleText.text = "Circle 1";
+
+        level = GameObject.Find("levelObject").GetComponent<LevelGeneration>();
+    }
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+        calculateTime();
+
+        // if the player has reached the circle boundary
+        if(score > circleBoundaries[circle])
+        {
+            updateCicle();
+        }
 	}
 
     void calculateTime()
     {
-        timeElapsed = Time.deltaTime;
+        timeElapsed = Time.realtimeSinceStartup;
+        timeText.text = timeElapsed.ToString("F2");
+    }
+
+    void calculateScore()
+    {
+        score += 5;
+        scoreText.text = "Score " + score.ToString();
+    }
+
+    // change the textures etc for the next circle
+    void updateCicle()
+    {
+        ++circle;
+        circleText.text = "Circle " + circle;
+
+        // the change platform material function needs to be changed to change sprite
+        //level.changePlatformSprite(platformSprites[circle]);
+
+        // reduce the platforms frequency and size
+        //level.changePlatformFrequency(level.initialWaitTime - 0.5);
+        //level.changePlatformWidthRange(level.minPlatformWidth - 0.5, level.maxPlatformWidth - 0.5);
+
+    }
+
+    public void resetGame()
+    {
+        Application.LoadLevel(Application.loadedLevel);
     }
 }
---
+++ GGJ16/Assets/killPlayer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class killPlayer : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if(GameObject.Find("Player") == coll.gameObject)
+        {
+            GameObject.Find("GameManager").GetComponent<gameManager>().resetGame();
+        }
+    }
+}
